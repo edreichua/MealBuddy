51fd6b4ac73d39a5927546db430d87607e2f42f6
Edrei Chua <edreichua@hotmail.com>
2016-05-25 17:30:21 -0400

    GCM begins

---
+++ app/src/main/java/com/example/mealbuddy/mealbuddy/GcmRegistrationAsyncTask.java
@@ -0,0 +1,77 @@
+package com.example.mealbuddy.mealbuddy;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.mealbuddy.myapplication.backend.registration.Registration;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by edreichua on 5/13/16.
+ */
+public class GcmRegistrationAsyncTask extends AsyncTask<Void, Void, String> {
+
+    private static Registration regService = null;
+    private GoogleCloudMessaging gcm;
+    private Context context;
+
+    public GcmRegistrationAsyncTask(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+
+        // Register the device with the server
+        if (regService == null) {
+            Registration.Builder builder = new Registration.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    .setRootUrl("https://ultra-badge-131020.appspot.com/_ah/api");
+            regService = builder.build();
+        }
+
+        String message = "";
+        try {
+            if (gcm == null) {
+                // Set the GCM
+                gcm = GoogleCloudMessaging.getInstance(context);
+            }
+
+            // Register the device with sender ID
+            String regId = gcm.register(Globals.SENDER_ID);
+            message = "Device registered, registration ID=" + regId;
+            Log.d("Testing", "Message: " + message);
+
+            // Execute the registration
+            regService.register(regId).execute();
+
+            // Initialise the regID in Globals
+            Globals.regID = regId;
+
+        } catch (IOException ex) {
+
+            // Print error message if registration fails
+            ex.printStackTrace();
+            message = "Error: " + ex.getMessage();
+        }
+        return message;
+    }
+
+    @Override
+    protected void onPostExecute(String msg) {
+
+        // Print a toast to inform user on registration status
+        Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+
+        // Insert a log, too
+        Logger.getLogger("REGISTRATION").log(Level.INFO, msg);
+    }
+}
---
+++ app/src/main/java/com/example/mealbuddy/mealbuddy/Globals.java
@@ -0,0 +1,17 @@
+package com.example.mealbuddy.mealbuddy;
+
+/**
+ * Created by edreichua on 5/14/16.
+ * A number of global static final variables to be accessed
+ * for various functions throughout the app
+ */
+public class Globals {
+
+    // Datastore for GCM
+
+
+    // Server utilities
+    public static final String SENDER_ID = "1040154061256";
+    public static final String URL = "https://ultra-badge-131020.appspot.com";
+    public static String regID = null;
+}
